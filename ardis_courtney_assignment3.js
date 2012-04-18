247efeb7bdf579008f6867166b3e7008739e9403
ardiscb <ardis.courtney@gmail.com>
2012-04-18 14:05:10 -0300

    Update ardis_courtney_assignment3.js

--- ardis_courtney_assignment3.js
+++ ardis_courtney_assignment3.js
@@ -1,35 +1,78 @@
-//Family of settlers traveling the Old West
+//Characters copyrighted by DC Comics.
+
+/*
+Add and/or label:
+JSON data
+Argument: Array
+Argument: Number
+Arugment: String
+Argument: Object
+Argument: Boolean
+Conditional
+Nested Conditional
+For loop
+While loop
+Nested loop
+Math
+Returns
+Back
+Property: Object
+*/
 
 //Clark Kent
 var clark = {
+	//Property: String
 	name: "Clark Kent",
+	//Property: Number
 	age: 18
-	
+	//Method: Function
+	doSomething: function() {
+		//Return: ?
+		return something;
+	}
 };
 	
 
 //Pa Kent
 var pa = {
+	//Property: String
 	name: "Jonathan Kent",
+	//Property: Number
 	age: 42,
+	//Property: Boolean
 	bearded: true
 };
 
 //Ma Kent
 var ma = {
+	//Property: String
 	name: "Martha Kent",
+	//Property: Number
 	age: 40,
+	//Method: Procedure
 	isHappy: function(happyClark) {
 		happyClark = true;
 		if (happyClark === true) {
 		happyMartha = true;
+		//Output
 		console.log("Martha is happy!");
 		}
 		else {
 		happyMartha = false;
+		//Output
 		console.log("Martha is not happy!");
 		};
 	}
 };
 
+var something = {
+	//Method: Accessor
+	getSomething: function() {}
+	//Method: Mutator
+	setSomething: function() {}
+};
+
+
 ma.isHappy();
+
+
